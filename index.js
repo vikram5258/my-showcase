9f1226788b2f2e10564ba5d6ee14f5a4b83601a5
vikram <vikram@thinkebiz.net>
2021-05-23 23:50:54 +0530

    route added

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
+  console.info("/hello ");
+  res.send("just checking");
+});
+app.get("/hello", (req, res, next) => {
   console.info("/hello call success ");
   res.send("just checking");
 });
-
 app.listen(process.env.PORT, () => {
   console.info("Server is running on PORT:", process.env.PORT);
 });
