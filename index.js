03df3d2160a9e2f270490d131facc031dee4e507
vikram <vikram@thinkebiz.net>
2021-05-23 23:58:14 +0530

    route added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res, next) => {
 });
 app.get("/hello", (req, res, next) => {
   console.info("/hello call success ");
-  res.send("just checking");
+  res.send("<h1>just checking</h1>");
 });
 app.listen(process.env.PORT, () => {
   console.info("Server is running on PORT:", process.env.PORT);
